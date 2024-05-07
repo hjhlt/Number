27faf97af8742e231fc33df52fb5e7904cb69126
hjhlt <1226145893@qq.com>
2024-05-08 01:22:52 +0800

    用户界面

--- test.py
+++ test.py
@@ -26,7 +26,7 @@ def maxPool(img):
     for i in range(28):
         for j in range(28):
             result[i, j] = np.min(img[i * x // 28:(i + 1) * x // 28, j * y // 28:(j + 1) * y // 28])
-            if (result[i, j] > 0.5):
+            if result[i, j] > 0.3:
                 result[i, j] = 1
             else:
                 result[i, j] = 0
@@ -35,37 +35,29 @@ def maxPool(img):
 
 def solve(image):
     # 使用PIL读取图片
-    image = cv.imread(image)
-    # 使用matplotlib显示图片
-    plt.imshow(image, cmap='gray')
-    # 显示图片
-    plt.show()
-    #将图片变为灰度图片
+    # image = cv.imread(image)
+    # 将图片变为灰度图片
     image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    #将像素值变为0-1之间方便神经网络进行预测
+    # 将像素值变为0-1之间方便神经网络进行预测
     image = image / 255
-    #进行最大池化特征提取并将图片压缩到28x28的大小
+    # 进行最大池化特征提取并将图片压缩到28x28的大小
     image = maxPool(image)
-    #将黑白颠倒
+    # 将黑白颠倒
     image = [[1 - num for num in row] for row in image]
     image = np.array(image)
-    image = torch.from_numpy(image).float()
+    # image = torch.from_numpy(image).float()
     # 使用matplotlib显示图片
-    plt.imshow(image, cmap='gray')
+    # plt.imshow(image, cmap='gray')
     # 显示图片
-    plt.show()
-    image = image.unsqueeze(0)
-    image = image.unsqueeze(0)
+    # plt.show()
+    # image = image.unsqueeze(0)
+    # image = image.unsqueeze(0)
     return image
 
-nW=tkinter.Tk()
-nW.geometry('500x600')
-nW.title('手写数字识别')
-nW.mainloop()
-for i in range(1):
-    x = 'test'
-    image_name = "shouxie.jpg"
-    image = solve(image_name)
-    output = network(image)
-    pred = output.data.max(1, keepdim=True)
-    print(pred)
+# for i in range(1):
+#     x = 'test'
+#     image_name = "shouxie.jpg"
+#     image = solve(image_name)
+#     output = network(image)
+#     pred = output.data.max(1, keepdim=True)
+#     print(pred)
