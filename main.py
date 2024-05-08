d9266af5e731fbbf3c3c5ad62a099c91e721d605
heard <heard@duomai.com>
2024-05-08 16:45:40 +0800

    ok

--- main.py
+++ main.py
@@ -1,134 +1,165 @@
+import cv2
+from PIL import Image, ImageTk
+import tkinter
+from pre import solve
+import numpy as np
 import torch
-import torchvision
-from torch.utils.data import DataLoader
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import matplotlib.pyplot as plt
-from PIL import Image
-import cv2 as cv
 from Net import Net
+from tkinter import filedialog
+from tkinter import font
 
+# 创建一个TK界面
+root = tkinter.Tk()
+root.geometry("1600x900")
+root.resizable(False, False)
+root.title('手写数字识别')
 
-n_epochs = 1
-batch_size_train = 60
-batch_size_test = 1000
-learning_rate = 0.00001
-momentum = 0.5
-log_interval = 10
-random_seed = 1
-torch.manual_seed(random_seed)
-
-train_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST('./data/', train=True, download=True,
-                               transform=torchvision.transforms.Compose([
-                                   torchvision.transforms.ToTensor(),
-                                   # torchvision.transforms.Normalize(
-                                   #     (0.1307,), (0.3081,))
-                               ])),
-    batch_size=batch_size_train, shuffle=True)
-test_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST('./data/', train=False, download=True,
-                               transform=torchvision.transforms.Compose([
-                                   torchvision.transforms.ToTensor(),
-                                   # torchvision.transforms.Normalize(
-                                   #     (0.1307,), (0.3081,))
-                               ])),
-    batch_size=batch_size_test, shuffle=True)
-
-examples = enumerate(test_loader)
-batch_idx, (example_data, example_targets) = next(examples)
-# print(example_targets)
-# print(example_data.shape)
-
-
-fig = plt.figure()
-for i in range(6):
-    plt.subplot(2, 3, i + 1)
-    plt.tight_layout()
-    plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
-    plt.title("Ground Truth: {}".format(example_targets[i]))
-    plt.xticks([])
-    plt.yticks([])
-plt.show()
+video = cv2.VideoCapture(0)
+res = video.set(3, 1280)
 
 network = Net()
-optimizer = optim.SGD(network.parameters(), lr=learning_rate, momentum=momentum)
 
-network_state_dict = torch.load('model.pth')
+network_state_dict = torch.load('./model/model.pth')
 network.load_state_dict(network_state_dict)
-optimizer_state_dict = torch.load('optimizer.pth')
-optimizer.load_state_dict(optimizer_state_dict)
+network.eval()
 
-train_losses = []
-train_counter = []
-test_losses = []
-test_counter = [i * len(train_loader.dataset) for i in range(n_epochs + 1)]
+def pre(img):
+    x, y, z = img.shape
+    return img[x // 2 - 350:x // 2 + 349, y // 2 - 350:y // 2 + 349]
 
 
-def train(epoch):
-    network.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        optimizer.zero_grad()
-        output = network(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch, batch_idx * len(data),
-                                                                           len(train_loader.dataset),
-                                                                           100. * batch_idx / len(train_loader),
-                                                                           loss.item()))
-            train_losses.append(loss.item())
-            train_counter.append((batch_idx * 64) + ((epoch - 1) * len(train_loader.dataset)))
-            torch.save(network.state_dict(), './model.pth')
-            torch.save(optimizer.state_dict(), './optimizer.pth')
+def enlarge(img):
+    result = np.zeros((700, 700))
+    for i in range(28):
+        for j in range(28):
+            result[i * 25:(i + 1) * 25, j * 25:(j + 1) * 25] = img[i][j]
+    return result
 
 
-def test():
-    network.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            output = network(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()
-            pred = output.data.max(1, keepdim=True)[1]
-            correct += pred.eq(target.data.view_as(pred)).sum()
-    test_loss /= len(test_loader.dataset)
-    test_losses.append(test_loss)
-    print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+cnt = 0
+mode = 0
+first = 1
+
+def imshow():
+    global video
+    global root
+    global image1
+    global image2
+    global ans
+    global cnt
+    global mode
+    r, img = video.read()
+    img = pre(img)
+
+    if r and mode == 1:
+        # 将adarray转化为image
+        img1 = Image.fromarray(img)
+        # 显示图片到label
+        img1 = ImageTk.PhotoImage(img1)
+
+        img2 = solve(img) * 255
+        img2 = enlarge(img2)
+        img2 = Image.fromarray(img2)
+        img2 = ImageTk.PhotoImage(img2)
+        image1.image = img1
+        image1['image'] = img1
+        image2.image = img2
+        image2['image'] = img2
+        img3 = solve(img)
+        image = torch.from_numpy(img3).float()
+        image = image.unsqueeze(0)
+        image = image.unsqueeze(0)
+        output = network(image)
+        pred = output.data.max(1, keepdim=True)
+        answer = pred[1][0][0].item()
+        if pred[0][0][0].item() > -0.001:
+            ans.config(text="数字为" + str(answer))
+            cnt = 0
+        else:
+            cnt += 1
+        if cnt > 60:
+            ans.config(text="未识别到数字")
+
+    # 创建一个定时器，每10ms进入一次函数
+    root.after(50, imshow)
 
 
-#train(1)
-#
-test()  # 不加这个，后面画图就会报错：x and y must be the same size
-for epoch in range(1, n_epochs + 1):
-    train(epoch)
-    test()
+def open_image():
+    global video
+    global root
+    global image1
+    global image2
+    global ans
+    global cnt
+    global mode
+    # 使用filedialog打开文件选择对话框
+    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif;*.bmp")])
+    if file_path:
+        mode = 0
+        # 使用Pillow读取图片
+        img = Image.open(file_path)
+        # 使用BICUBIC作为重采样过滤器
+        img = img.resize((700, 700), Image.BICUBIC)
+        # 将PIL Image对象转换为Tkinter可以显示的PhotoImage对象
+        photo = ImageTk.PhotoImage(img)
+
+        img = np.array(img)
+        img2 = solve(img) * 255
+        img2 = enlarge(img2)
+        img2 = Image.fromarray(img2)
+        img2 = ImageTk.PhotoImage(img2)
+
+        img3 = solve(img)
+        image = torch.from_numpy(img3).float()
+        image = image.unsqueeze(0)
+        image = image.unsqueeze(0)
+        output = network(image)
+        pred = output.data.max(1, keepdim=True)
+        answer = pred[1][0][0].item()
+
+        # 将上传的图片显示到窗口中
+        image1.image = photo
+        image1['image'] = photo
+        image2.image = img2
+        image2['image'] = img2
+
+        if answer > -0.001:
+            ans.config(text="数字为" + str(answer))
+        else:
+            ans.config(text="未识别到数字")
 
 
+def start():
+    global mode
+    global first
+    mode = 1
+    if first == 1:
+        imshow()
+        first = 0
+
+label1 = tkinter.Label(root, text='原图像')
+label2 = tkinter.Label(root, text='预处理后的图像')
+
+label1.place(x=0, y=0)
+label2.place(x=900, y=0)
+# 创建label标签
+image1 = tkinter.Label(root, text=' ', width=700, height=700)
+image1.place(x=0, y=25, width=700, height=700)
+
+image2 = tkinter.Label(root, text=' ', width=700, height=700)
+image2.place(x=900, y=25, width=700, height=700)
+
+my_font = font.Font(size=15)
+ans = tkinter.Label(root, text='未识别到数字',font=my_font)
+ans.place(x=750, y=775, width=200, height=100)
+
+btn_open = tkinter.Button(root, text="上传图片", command=open_image)
+btn_open.place(x=200, y=800, width=100, height=50)
+btn_video = tkinter.Button(root, text="使用摄像头捕捉", command=start)
+btn_video.place(x=0, y=800, width=100, height=50)
 
 
-fig = plt.figure()
-plt.plot(train_counter, train_losses, color='blue')
-plt.scatter(test_counter, test_losses, color='red')
-plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
-plt.xlabel('number of training examples seen')
-plt.ylabel('negative log likelihood loss')
+root.mainloop()
 
-examples = enumerate(test_loader)
-batch_idx, (example_data, example_targets) = next(examples)
-with torch.no_grad():
-    output = network(example_data)
-fig = plt.figure()
-for i in range(6):
-    plt.subplot(2, 3, i + 1)
-    plt.tight_layout()
-    plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
-    plt.title("Prediction: {}".format(output.data.max(1, keepdim=True)[1][i].item()))
-    plt.xticks([])
-    plt.yticks([])
-plt.show()
+# 释放video资源
+video.release()
---
+++ pre.py
@@ -0,0 +1,57 @@
+import torch
+import torchvision
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from PIL import Image
+import cv2 as cv
+from Net import Net
+import numpy as np
+import tkinter
+
+network = Net()
+
+network_state_dict = torch.load('./model/model.pth')
+network.load_state_dict(network_state_dict)
+
+network.eval()
+
+
+def maxPool(img):
+    img = np.array(img)
+    x, y = img.shape
+    result = np.zeros((28, 28))
+    for i in range(28):
+        for j in range(28):
+            result[i, j] = np.min(img[i * x // 28:(i + 1) * x // 28, j * y // 28:(j + 1) * y // 28])
+            if result[i, j] > 0.5:
+                result[i, j] = 1
+            else:
+                result[i, j] = 0
+    return result
+
+
+def solve(image):
+    # 使用PIL读取图片
+    # image = cv.imread(image)
+    # 将图片变为灰度图片
+    image = cv.cvtColor(np.array(image), cv.COLOR_RGB2BGR)
+    if image.shape[2] == 3:
+        image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    # 将像素值变为0-1之间方便神经网络进行预测
+    image = image / 255
+    # 进行最大池化特征提取并将图片压缩到28x28的大小
+    image = maxPool(image)
+    # 将黑白颠倒
+    image = [[1 - num for num in row] for row in image]
+    image = np.array(image)
+    # image = torch.from_numpy(image).float()
+    # 使用matplotlib显示图片
+    # plt.imshow(image, cmap='gray')
+    # 显示图片
+    # plt.show()
+    # image = image.unsqueeze(0)
+    # image = image.unsqueeze(0)
+    return image
